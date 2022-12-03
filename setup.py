07ab80ce39a09d3b8784b0cadc47378403b4a174
Balan Artiom <artiombalan331@gmail.com>
2022-12-03 20:38:44 +0200

    made it run as script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name = "aoc_test",
+      version = "0.1",
+      description = "Test your Advent of Code solutions",
+      author='Balan Artiom',
+      license='MIT',
+      packages=['src/aoc_test'],
+      scripts=['src/aoc_test/aoct'],
+      zip_safe=False)
